1e4185366a54c15d072624eb379bba2b19de0b90
Ben Letchford <contact@benl.com.au>
2020-09-17 16:39:10 +1000

    Website deployment

---
+++ precache-manifest.0625c0991bcc560e5f8626e55a7e9d22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1308ceac3fcdb19b1ae86517961d1298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b7e318b875318398ae",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d0f05576478685f1ac9f",
+    "url": "/static/css/main.a2a133fa.chunk.css"
+  },
+  {
+    "revision": "86b7e318b875318398ae",
+    "url": "/static/js/2.3c0c0eb7.chunk.js"
+  },
+  {
+    "revision": "d3651c946796eb2d839d7d6b8a1dbaad",
+    "url": "/static/js/2.3c0c0eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f05576478685f1ac9f",
+    "url": "/static/js/main.372a50f9.chunk.js"
+  },
+  {
+    "revision": "60783aa6e701735bf302",
+    "url": "/static/js/runtime-main.e554679e.js"
+  },
+  {
+    "revision": "b4fac47c4388d5d9b62feecf463e2079",
+    "url": "/static/media/logo.b4fac47c.svg"
+  }
+]);
